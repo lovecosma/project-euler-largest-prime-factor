a18ea16cbd92d306019dfdaa2a6bcec7012698f4
Matthew Williams <love.cosma@gmail.com>
2021-07-27 21:42:40 -0600

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,28 @@
 # Enter your procedural solution here!
+def largest_prime_factor(num)
+
+    maxPrime = -1
+    
+    while(num % 2 == 0)
+        maxPrime = 2
+        num = num / 2
+    end 
+
+    counter = 3
+    while counter <= Math.sqrt(num)
+
+       if(num % counter == 0)
+            maxPrime = counter
+            num = num / counter 
+       end 
+
+        counter += 2
+    end 
+
+    if (num > 4)
+        maxPrime = num;
+    end 
+
+    return maxPrime
+
+end 
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,35 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_accessor :input
+    def initialize(input)
+        @input = input
+    end 
+
+    def number
+
+        maxPrime = -1
+
+        while @input % 2 == 0
+            maxPrime = 2
+            @input = @input / 2
+        end 
+
+        counter = 3
+
+        while counter <= Math.sqrt(@input)
+            if @input % counter == 0
+                maxPrime = counter
+                @input = @input / counter 
+            end
+            counter += 2
+        end 
+
+        if @input > 4
+            maxPrime = @input
+        end 
+
+        return maxPrime
+    end 
+
+    
+end 
